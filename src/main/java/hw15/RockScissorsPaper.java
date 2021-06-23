fd6ac18abcd272112f7502e2704d9c4aeae33640
natashasavytska <natashasavytska@gmail.com>
2021-06-23 23:36:13 +0300

    hw 17.2 logs added

--- src/main/java/hw15/RockScissorsPaper.java
+++ src/main/java/hw15/RockScissorsPaper.java
@@ -1,9 +1,12 @@
 package hw15;
 
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static hw15.GameChoices.*;
 
@@ -11,6 +14,11 @@ public class RockScissorsPaper {
 
     private static final Map<GameChoices, GameChoices> BEATS = new HashMap<>();
     private static final Scanner sc = new Scanner(System.in);
+    static String baseName = "C:\\Users\\Nataly\\Documents\\Hillel\\Odessa_Java_Introduction" +
+            "\\Java_Introduction_homeworks\\src\\main\\java\\hw15";
+    static String dirNameForGameResult = baseName + "\\gameResults";
+    static String dirNameForGameLogs = baseName + "\\gameLogs";
+
 
     static {
         BEATS.put(PAPER, ROCK);
@@ -18,19 +26,46 @@ public class RockScissorsPaper {
         BEATS.put(SCISSORS, PAPER);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         User computer = new User("Computer");
         User user = createPlayerProfile();
+
+        File dirForGameResult = new File(dirNameForGameResult);
+        dirForGameResult.mkdir();
+        File dirForGameLogs = new File(dirNameForGameLogs);
+        dirForGameLogs.mkdir();
+        String fileNameGameResult = "result_" + user.getName().toLowerCase() + "_" +
+                new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+        String createFileGameResult = dirNameForGameResult + "\\" + fileNameGameResult + ".txt";
+        File gameResult = new File(createFileGameResult);
+        if (!gameResult.exists())
+            gameResult.createNewFile();
+        OutputStream gameResults = new FileOutputStream(createFileGameResult);
+        String fileNameGameLogs = "log_" + user.getName().toLowerCase() + "_" +
+                new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+        String createFileGameLogs = dirNameForGameLogs + "\\" + fileNameGameLogs + ".txt";
+        File gameLogsFile = new File(createFileGameLogs);
+        if (!gameLogsFile.exists())
+            gameLogsFile.createNewFile();
+        OutputStream gameLogs = new FileOutputStream(createFileGameLogs, true);
+
         int numberOfGames = chooseNumberOfGames(user);
+        gameLogs.write((user.getName() + " choose to play " + numberOfGames + " games." + "\n")
+                .getBytes(StandardCharsets.UTF_8));
         for (int i = 0; i < numberOfGames; i++) {
-            playGame(user, computer);
+            gameLogs.write(("Game " + (1 + i)).getBytes(StandardCharsets.UTF_8));
+            playGame(user, computer, gameLogs);
             if (i < numberOfGames - 1) {
-                if (checkToContinue() == 0)
+                if (checkToContinue() == 0) {
+                    gameLogs.write(("\n" + user.getName() + " decided to stop the game.")
+                            .getBytes(StandardCharsets.UTF_8));
                     break;
+                }
             }
         }
-        writeFinalResults(user, computer);
+        System.out.println(writeFinalResults(user, computer));
+        gameResults.write(writeFinalResults(user, computer).getBytes(StandardCharsets.UTF_8));
     }
 
     private static User createPlayerProfile() {
@@ -71,12 +106,16 @@ public class RockScissorsPaper {
         }
     }
 
-    private static void playGame(User user, User computer) {
+    private static void playGame(User user, User computer, OutputStream outputStream) throws IOException {
         GameChoices userChoice = userMakesChoice();
-        System.out.println("Your choice is: " + userChoice);
+        String userTurn = "\n" + user.getName() + "'s choice is: " + userChoice + "\n";
+        System.out.print(userTurn);
+        outputStream.write(userTurn.getBytes(StandardCharsets.UTF_8));
         GameChoices computerChoice = generationOfComputerChoice();
-        System.out.println("Computer's choice is: " + computerChoice);
-        findOneGameResult(userChoice, computerChoice, user, computer);
+        String computerTurn = "Computer's choice is: " + computerChoice + "\n";
+        System.out.print(computerTurn);
+        outputStream.write(computerTurn.getBytes(StandardCharsets.UTF_8));
+        findOneGameResult(userChoice, computerChoice, user, computer, outputStream);
 
     }
 
@@ -90,36 +129,39 @@ public class RockScissorsPaper {
             }
             return answer;
         } catch (InputMismatchException e) {
+            sc.nextLine();
             System.out.println("Please, choose 1 for YES or 0 for NO");
             return checkToContinue();
         }
     }
 
     private static void findOneGameResult(GameChoices userChoice, GameChoices computerChoice,
-                                          User user, User computer) {
+                                          User user, User computer, OutputStream outputStream) throws IOException {
         if (userChoice == computerChoice) {
-            System.out.println("Its a tie");
+            System.out.println("It's a tie");
+            outputStream.write(("It's a tie" + "\n").getBytes(StandardCharsets.UTF_8));
         } else if (BEATS.get(userChoice) == computerChoice) {
             System.out.println("You've won");
             user.setScore(user.getScore() + 1);
+            outputStream.write((user.getName() + "'s won" + "\n").getBytes(StandardCharsets.UTF_8));
         } else {
             System.out.println("You've lost");
             computer.setScore(computer.getScore() + 1);
+            outputStream.write((user.getName() + "'s lost" + "\n").getBytes(StandardCharsets.UTF_8));
         }
 
     }
 
-    private static void writeFinalResults(User user, User computer) {
-        System.out.println("========== Game result ==========");
-        System.out.println("Computer score: " + computer.getScore());
-        System.out.println(user.getName() + "'s score: " + user.getScore());
-
+    private static String writeFinalResults(User user, User computer) {
         if (computer.getScore() > user.getScore()) {
-            System.out.println("Computer win!");
+            return "========== Game result ==========" + "\n" + "Computer score: " + computer.getScore() +
+                    "\n" + user.getName() + "'s score: " + user.getScore() + "\n" + "Computer win!";
         } else if (user.getScore() > computer.getScore()) {
-            System.out.println(user.getName() + " win!");
+            return "========== Game result ==========" + "\n" + "Computer score: " + computer.getScore() +
+                    "\n" + user.getName() + "'s score: " + user.getScore() + "\n" + user.getName() + " win!";
         } else {
-            System.out.println("It's a tie!");
+            return "========== Game result ==========" + "\n" + "Computer score: " + computer.getScore() +
+                    "\n" + user.getName() + "'s score: " + user.getScore() + "\n" + "It's a tie!";
         }
     }
 
